e46667d84c560382727360678f8be62db4c8c3aa
pietro aluffi <pietro.aluffi@icloud.com>
2020-12-03 23:13:12 +0000

    first

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ print('it works!')
 app = Flask(__name__)
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=to_app, trigger='interval', hours=3, start_date='2020-12-01 19:26:00', end_date='2100-06-15 11:00:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=3, start_date='2020-12-01 19:26:01', end_date='2100-06-15 11:00:00')
+scheduler.add_job(func=to_app, trigger='interval', hours=3, start_date='2020-12-01 20:53:00', end_date='2100-06-15 11:00:00')
+scheduler.add_job(func=to_app, trigger='interval', hours=3, start_date='2020-12-01 20:53:01', end_date='2100-06-15 11:00:00')
 
 scheduler.start()
 
@@ -17,8 +17,8 @@ def home():
     k = D
     return render_template('home-page_demo.html',d=k)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
 
 
 
