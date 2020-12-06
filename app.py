f04065d1e6620a384bd368f6a04a6e1e19147048
pietroaluffi <pietro.aluffi@icloud.com>
2020-12-06 10:28:00 +0000

    c

--- app.py
+++ app.py
@@ -6,25 +6,22 @@ print('it works!')
 app = Flask(__name__)
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 06:40:01', end_date='2100-06-15 09:05:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 06:40:02', end_date='2100-06-15 09:05:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 07:00:03', end_date='2100-06-15 11:00:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 07:00:04', end_date='2100-06-15 09:05:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 07:20:05', end_date='2100-06-15 09:05:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 07:20:06', end_date='2100-06-15 09:05:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 07:40:07', end_date='2100-06-15 11:00:00')
-scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 07:40:08', end_date='2100-06-15 09:05:00')
+scheduler.add_job(func=to_app, trigger='interval', hours=2, start_date='2020-12-06 10:30:01', end_date='2100-06-15 09:05:00')
+# scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 10:30:02', end_date='2100-06-15 09:05:00')
+# scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 10:00:03', end_date='2100-06-15 11:00:00')
+# scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 10:00:04', end_date='2100-06-15 09:05:00')
+# scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 10:20:05', end_date='2100-06-15 09:05:00')
+# scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 10:20:06', end_date='2100-06-15 09:05:00')
+# scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 10:40:07', end_date='2100-06-15 11:00:00')
+# scheduler.add_job(func=to_app, trigger='interval', hours=24, start_date='2020-12-06 10:40:08', end_date='2100-06-15 09:05:00')
 
 scheduler.start()
 
 
 @app.route('/')
 def home():
-    published = list(zip(IMAGE_TO_SLANDER,DATE,TITLE,ARTICLE))
-    if len(published) >= 8:
-        return render_template('home-page_demo.html',type=type,published = published )
-    else:
-        return render_template('popup.html')
+    return render_template('home-page_demo1.html',type=type,published = published )
+
 
 @app.template_global(name='zip')
 def _zip(*args, **kwargs): #to not overwrite builtin zip in globals
